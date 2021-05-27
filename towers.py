297d3d7a8281e9d272ca23390c18ff0d9b906f37
Hussen <hussenmohammedak@gmail.com>
2021-05-26 22:45:04 -0400

    Simpler print statements

--- towers.py
+++ towers.py
@@ -1,15 +1,10 @@
 def towers(n, start, end, middle):
     if n == 1:
-        print("Move %i from tower %s to tower %s" % (n, start, end))
-        # print(f"Move {n} from tower {start} to tower {end}")
-        # moves += 1
+        print(f"Move {n} from tower {start} to tower {end}")
     else:
         towers(n-1, start, middle, end)
-        print("Move %i from tower %s to tower %s" % (n, start, end))
-        # print(f"Move {n} from tower {start} to tower {end}")
-        # moves += 1
+        print(f"Move {n} from tower {start} to tower {end}")
         towers(n-1, middle, end, start)
 
-    # print(moves)
 
 towers(2, 'A', 'C', 'B')
